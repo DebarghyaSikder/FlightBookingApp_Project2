e560fd42d0e8b17cf46df7eb4364eeba81087667
Debarghya Sikder <debarghya1412@gmail.com>
2025-11-24 12:29:44 +0530

    Initial commit

---
+++ src/main/java/com/flightapp/model/enums/Gender.java
@@ -0,0 +1,7 @@
+package com.flightapp.model.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/test/java/com/flightapp/flight_webflux_mongo/FlightWebfluxMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightapp.flight_webflux_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightWebfluxMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
